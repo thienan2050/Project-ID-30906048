878049c49657ece324dfce7bb15537a8e585148c
thienan2050 <thienan2050>
2021-07-22 01:42:51 +0700

    Remove spaces

--- main.py
+++ main.py
@@ -26,40 +26,40 @@ with open('data.csv', 'r') as file:
         if row[0] == '----------- Personal Login -----------':
             total += 1
         if (index % 33) == 6:
-            fullName.append(row[0].split(':')[1])
+            fullName.append(row[0].split(':')[1].strip())
         if (index % 33) == 7:
-            DOBirth.append(row[0].split(':')[1])
+            DOBirth.append(row[0].split(':')[1].strip())
         if (index % 33) == 8:
-            emailAddress.append(row[0].split(':')[1])
+            emailAddress.append(row[0].split(':')[1].strip())
         if (index % 33) == 9:
-            address.append(row[0].split(':')[1])
+            address.append(row[0].split(':')[1].strip())
         if (index % 33) == 10:
-            phoneNumber.append(row[0].split(':')[1])
+            phoneNumber.append(row[0].split(':')[1].strip())
         if (index % 33) == 11:
-            town.append(row[0].split(':')[1])
+            town.append(row[0].split(':')[1].strip())
         if (index % 33) == 19:
-            cardNumber.append(row[0].split(':')[1] )
+            cardNumber.append(row[0].split(':')[1].strip().replace(' ',''))
         if (index % 33) == 20:
-            cardExp.append(row[0].split(':')[1])
+            cardExp.append(row[0].split(':')[1].strip())
         if (index % 33) == 21:
-            securityCode.append(row[0].split(':')[1])
+            securityCode.append(row[0].split(':')[1].strip())
         if (index % 33) == 23:
-            sortCode.append(row[0].split(':')[1])
+            sortCode.append(row[0].split(':')[1].strip())
         if (index % 33) == 24:
-            account.append(row[0].split(':')[1])
+            account.append(row[0].split(':')[1].strip())
         if (index % 33) == 26:
-            submitted.append(row[0].split(':')[1])
+            submitted.append(row[0].split(':')[1].strip())
         if (index % 33) == 27:
-            location.append(row[2])
+            location.append(row[2].strip())
         
     
 
 for i in range(0, total):
-    print '{'+cardNumber[i]+'|'+cardExp[i]+'|'+securityCode[i]+'|'+fullName[i]+'|'+address[i]+'|'+town[i]+'|'+location[i]+'|'+phoneNumber[i]+'|'+emailAddress[i]+'|'+DOBirth[i]+'|'+sortCode[i]+'|'+account[i]+'|'+submitted[i]+'}'    
+    print cardNumber[i]+'|'+cardExp[i]+'|'+securityCode[i]+'|'+fullName[i]+'|'+address[i]+'|'+town[i]+'|'+location[i]+'|'+phoneNumber[i]+'|'+emailAddress[i]+'|'+DOBirth[i]+'|'+sortCode[i]+'|'+account[i]+'|'+submitted[i]    
 
 with open("output.txt",'w') as f:
     for i in range(0, total):
-        f.write('{'+cardNumber[i]+'|'+cardExp[i]+'|'+securityCode[i]+'|'+fullName[i]+'|'+address[i]+'|'+town[i]+'|'+location[i]+'|'+phoneNumber[i]+'|'+emailAddress[i]+'|'+DOBirth[i]+'|'+sortCode[i]+'|'+account[i]+'|'+submitted[i]+'}\n')    
+        f.write(cardNumber[i]+'|'+cardExp[i]+'|'+securityCode[i]+'|'+fullName[i]+'|'+address[i]+'|'+town[i]+'|'+location[i]+'|'+phoneNumber[i]+'|'+emailAddress[i]+'|'+DOBirth[i]+'|'+sortCode[i]+'|'+account[i]+'|'+submitted[i]+'\n')    
 
 
 
